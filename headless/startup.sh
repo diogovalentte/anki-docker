a0c0765b5afb7670f614b8c091e5d6ed68b629d8
Diogo Valentim <diogo@valentium.com.br>
2025-08-04 20:31:47 -0300

    feat: add optional env var ANKICONNECT_API_KEY in headless image

--- headless/startup.sh
+++ headless/startup.sh
@@ -9,4 +9,10 @@ echo "[i] Setting AnkiConnect wildcard webCorsOriginList and webBindAddress!"
 jq '.webCorsOriginList = ["*"] | .webBindAddress = "0.0.0.0"' $ANKICONNECT_CONFIG_FILE >tmp_file
 mv tmp_file $ANKICONNECT_CONFIG_FILE
 
+if [ -n "$ANKICONNECT_API_KEY" ]; then
+    echo "[i] Setting AnkiConnect API key!"
+    jq --arg key "$ANKICONNECT_API_KEY" '.apiKey = $key' "$ANKICONNECT_CONFIG_FILE" >tmp_file
+    mv tmp_file $ANKICONNECT_CONFIG_FILE
+fi
+
 anki -b /data
