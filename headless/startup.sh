9c3db793cbd39c4505160b79a04db3075913492b
Diogo Valentim <diogo@valentium.com.br>
2025-08-04 20:26:30 -0300

    fix: headless startup.sh log

--- headless/startup.sh
+++ headless/startup.sh
@@ -5,7 +5,7 @@ ANKICONNECT_CONFIG_FILE=$DATA_FOLDER/addons21/AnkiConnectDev/config.json
 mkdir -p $DATA_FOLDER/addons21
 ln -s -f /app/anki-connect/plugin $DATA_FOLDER/addons21/AnkiConnectDev
 
-echo "[i] Setting AnkiConnect wildcard webCorsOriginList!"
+echo "[i] Setting AnkiConnect wildcard webCorsOriginList and webBindAddress!"
 jq '.webCorsOriginList = ["*"] | .webBindAddress = "0.0.0.0"' $ANKICONNECT_CONFIG_FILE >tmp_file
 mv tmp_file $ANKICONNECT_CONFIG_FILE
 
