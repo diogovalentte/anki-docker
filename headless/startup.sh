36bd1fa99e6ea2854e30aaafe406c7f20372d660
Diogo Valentim <diogo@valentium.com.br>
2025-08-04 23:30:56 -0300

    feat: update startup.sh and others

--- headless/startup.sh
+++ headless/startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 DATA_FOLDER=/data
-ANKICONNECT_CONFIG_FILE=$DATA_FOLDER/addons21/AnkiConnectDev/config.json
+ANKICONNECTION_FOLDER=$DATA_FOLDER/addons21/AnkiConnectDev
+ANKICONNECT_CONFIG_FILE=$ANKICONNECTION_FOLDER/config.json
 
 mkdir -p $DATA_FOLDER/addons21
-ln -s -f /app/anki-connect/plugin $DATA_FOLDER/addons21/AnkiConnectDev
+rm -rf $ANKICONNECTION_FOLDER
+cp -r -a /app/anki-connect/plugin $ANKICONNECTION_FOLDER
 
 echo "[i] Setting AnkiConnect wildcard webCorsOriginList and webBindAddress!"
 jq '.webCorsOriginList = ["*"] | .webBindAddress = "0.0.0.0"' $ANKICONNECT_CONFIG_FILE >tmp_file
@@ -15,4 +17,4 @@ if [ -n "$ANKICONNECT_API_KEY" ]; then
     mv tmp_file $ANKICONNECT_CONFIG_FILE
 fi
 
-anki -b /data
+anki -b $DATA_FOLDER
